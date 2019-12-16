71b378727360003f4a4e0ac650e33a4da7679f39
Javier Acevedo <s0505752@tu-dresden.de>
2019-12-16 17:34:55 +0100

    Matrix multiplication exercise finished

---
+++ ExerciseIV/README.md
@@ -0,0 +1,31 @@
+
+For this exercise was implemented an algorithm of matrix multiplication using matrices of different sizes. The code is a C program which compares the execution time of the matrix multiplication using nested loops without multithreading, nested loops with multithreading, and finally a matrix multiplication using multithreading and SIngle Input Multiple Data (SIMD) instructions. The computing platform for this exercise was the following:
+
+```bash
+Architektur:                   x86_64
+CPU Operationsmodus:           32-bit, 64-bit
+Byte-Reihenfolge:              Little Endian
+CPU(s):                        8
+Liste der Online-CPU(s):       0-7
+Thread(s) pro Kern:            2
+Kern(e) pro Socket:            4
+Sockel:                        1
+NUMA-Knoten:                   1
+Anbieterkennung:               GenuineIntel
+Prozessorfamilie:              6
+Modell:                        94
+Modellname:                    Intel(R) Core(TM) i7-6700T CPU @ 2.80GHz
+Stepping:                      3
+CPU MHz:                       900.034
+Maximale Taktfrequenz der CPU: 3600,0000
+Minimale Taktfrequenz der CPU: 800,0000
+BogoMIPS:                      5616.00
+Virtualisierung:               VT-x
+L1d Cache:                     32K
+L1i Cache:                     32K
+L2 Cache:                      256K
+L3 Cache:                      8192K
+NUMA-Knoten0 CPU(s):           0-7
+
+```
+
---
+++ ExerciseIV/matrixmultiplication.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <x86intrin.h>
+#include <omp.h>
+
+/* Matrix dimensions configurations */
+#define N 1024
+#define M 2048
+#define K 512
+
+
+void mm_nomultithreading(float *matrixA, float *matrixB, float *result) {
+    /* Conventional nested loop for matrix multiplication */
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<K; k++) {
+            for(int j=0; j<M; j++) {
+                result[i*M+j] += matrixA[i*M+k]*matrixB[k*M+j];
+            }
+        }
+    }
+}
+
+void mm_multithreading(float *matrixA, float *matrixB, float *result) {
+    /* Conventional nested loop for matrix multiplication with parallel construct enabled */
+    #pragma omp parallel for
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<K; k++) {
+            for(int j=0; j<M; j++) {
+                result[i*M+j] += matrixA[i*M+k]*matrixB[k*M+j];
+            }
+        }
+    }
+}   
+
+void mm_multithreading_SIMD(float *matrixA, float *matrixB, float *result) {
+   /* */
+    #pragma omp parallel for
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<K; k++) {
+            __m128 broadcast_matrixA = _mm_set1_ps(matrixA[i*M+k]);
+            for(int j=0; j<M; j+=4) {
+                __m128 broadcast_result = _mm_load_ps(&result[i*M+j]);
+                __m128 broadcast_matrixB = _mm_load_ps(&matrixB[k*M+j]);
+                broadcast_result = _mm_add_ps(_mm_mul_ps(broadcast_matrixA,broadcast_matrixB),broadcast_result);
+                _mm_store_ps(&result[i*M+j], broadcast_result);
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    /* Input matrix definition*/
+    float *matrixA = _mm_malloc(N*M * sizeof *matrixA, 64);
+    float *matrixB = _mm_malloc(M*K * sizeof *matrixB, 64);
+
+    /* Resulting matrix definition */
+    float *result_nmult = _mm_malloc(N*K * sizeof *result_nmult, 64);
+    float *result_mult  = _mm_malloc(N*K * sizeof *result_mult, 64);
+    float *result_mult_simd = _mm_malloc(N*K * sizeof *result_mult_simd, 64);
+
+    /* Matrix initialization*/
+    for(int i=0; i<N*M; i++) matrixA[i] = 1.0*i;
+    for(int i=0; i<M*K; i++) matrixB[i] = 1.0*i;
+    //memset(matrixA, 0, N*M * sizeof *matrixA);
+    //memset(matrixB, 0, M*K * sizeof *matrixB);
+    memset(result_nmult, 0, N*K * sizeof *result_nmult);
+    memset(result_mult , 0, N*K * sizeof *result_mult);
+    memset(result_mult_simd, 0, N*K * sizeof *result_mult_simd);
+    double dtime;
+
+    /* Processing and benchmarking*/
+    
+    dtime = -omp_get_wtime();
+    mm_nomultithreading(matrixA,matrixB,result_nmult);
+    dtime += omp_get_wtime();
+    printf("Execution time NO multithreading: %f\n", dtime);
+    
+    
+    dtime = -omp_get_wtime();
+    mm_multithreading(matrixA,matrixB,result_mult);
+    dtime += omp_get_wtime();
+    printf("Execution time multithreading: %f\n", dtime);
+    
+    dtime = -omp_get_wtime();
+    mm_multithreading_SIMD(matrixA,matrixB,result_mult_simd);
+    dtime += omp_get_wtime();
+    printf("Execution time multithreading and SIMD : %f\n", dtime);
+  
+    /*Error flags*/ 
+    if(memcmp(result_nmult,result_mult, N*K*sizeof *result_nmult) != 0)
+	printf("Error: Memory content mismatch\n");
+    
+    if(memcmp(result_nmult,result_mult_simd, N*K*sizeof *result_nmult) != 0)
+ 	printf("Error: Memory content mismatch\n");
+
+    return 0;
+}
